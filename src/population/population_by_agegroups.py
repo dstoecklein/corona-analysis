535c893ff0a374f47b9170423bc7b633a67126d0
Daniel Stoecklein <daniel.stoecklein@student.fhws.de>
2021-11-22 21:23:42 +0100

    bugfix to get_population_by_agegroups function

--- src/population/population_by_agegroups.py
+++ src/population/population_by_agegroups.py
@@ -3,79 +3,67 @@ import eurostat
 from src.database import db_helper as database
 from src.utils import estat_helper
 
-if __name__ == '__main__':
-    db_proj = database.ProjDB()
-
-    df = eurostat.get_data_df(
-        'demo_r_d2jan',
-        flags=False
-    )
-
-    # clearing some usual eurostat stuff
-    df = estat_helper.clear_estat_data(df)
-
-    df = df.query(
-        '''
-        geo == @countries & geo != 'DE_TOT' \
-        & age != 'TOTAL' & age !='Y_GE75' & age != 'Y80-84' & age != 'Y_GE85' \
-        & sex == 'T' 
-        '''
-    )
-
-    df.to_csv("test.csv", sep=";")
-
 
 def population_by_agegroups(table: str, countries: list, starting_year: int = 1990):
     # Datasource: Eurostat
 
     db_proj = database.ProjDB()
 
-    df = eurostat.get_data_df(
+    tmp = eurostat.get_data_df(
         'demo_pjangroup',
         flags=False
-    )
+    ).copy()
 
     # clearing some usual eurostat stuff
-    df = estat_helper.clear_estat_data(df)
+    tmp = estat_helper.clear_estat_data(tmp)
 
-    df = df.query(
-        '''
-        geo == @countries & geo != 'DE_TOT' \
-        & age != 'TOTAL' & age !='Y_GE75' & age != 'Y80-84' & age != 'Y_GE85' \
-        & sex == 'T' 
-        '''
-    )
+    if countries:
+        tmp = tmp.query(
+            '''
+            geo == @countries \
+            & age != 'TOTAL' & age !='Y_GE75' & age != 'Y80-84' & age != 'Y_GE85' \
+            & sex == 'T' 
+            '''
+        )
+    else:
+        tmp = tmp.query(
+            '''
+            geo.str.len() == 2 \
+            & age != 'TOTAL' & age !='Y_GE75' & age != 'Y80-84' & age != 'Y_GE85' \
+            & sex == 'T' 
+            '''
+        )
 
     # melting to years
-    df = df.melt(
+    tmp = tmp.melt(
         id_vars=['age', 'sex', 'unit', 'geo'],
         var_name='year',
         value_name='population'
     )
 
     # assign 10-year agegroups
-    df = df.assign(
-        agegroup_10y=df['age'].map(
+    tmp = tmp.assign(
+        agegroup_10y=tmp['age'].map(
             estat_helper.ANNUAL_POPULATION_AGEGROUP_10Y_MAP
         )
     ).fillna('UNK')
 
-    df['year'] = pd.to_numeric(df['year'], errors='coerce')
+    tmp['year'] = pd.to_numeric(tmp['year'], errors='coerce')
 
     # only population from year 2010 - today
-    df = df[df['year'] >= starting_year]
+    tmp = tmp[tmp['year'] >= starting_year]
 
     # merge foreign keys
-    df = db_proj.merge_agegroups_fk(df, left_on='agegroup_10y', interval='10y')
-    df = db_proj.merge_calendar_years_fk(df, left_on='year')
-    df = db_proj.merge_countries_fk(df, left_on='geo', country_code='iso_3166_1_alpha2')
+    tmp = db_proj.merge_agegroups_fk(tmp, left_on='agegroup_10y', interval='10y')
+    tmp = db_proj.merge_calendar_years_fk(tmp, left_on='year')
+    tmp = db_proj.merge_countries_fk(tmp, left_on='geo', country_code='iso_3166_1_alpha2')
 
-    del df['age']
-    del df['geo']
-    del df['sex']
-    del df['unit']
+    del tmp['age']
+    del tmp['geo']
+    del tmp['sex']
+    del tmp['unit']
 
-    df = df.groupby(
+    tmp = tmp.groupby(
         [
             'countries_fk',
             'calendar_years_fk',
@@ -83,6 +71,7 @@ def population_by_agegroups(table: str, countries: list, starting_year: int = 19
         ], as_index=False
     )['population'].sum()
 
-    db_proj.insert_or_update(df, table)
+
+    db_proj.insert_or_update(tmp, table)
 
     db_proj.db_close()
